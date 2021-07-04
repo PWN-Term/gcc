160cbc5bd834a651bbf8938021183f510eeb275d
Martin Valba <kailing2@live.com>
2021-07-04 13:56:53 +0000

    Changes for build.sh

--- build.sh
+++ build.sh
@@ -9,19 +9,34 @@ mkdir -p out
 cd out
 
 # Configure
-./../gcc/configure --enable-languages=c,c++,fortran --with-system-zlib --disable-multilib --target=aarch64-linux-android --with-gmp=/data/data/hilled.pwnterm/files/usr --with-mpfr=/data/data/hilled.pwnterm/files/usr --with-mpc=/data/data/hilled.pwnterm/files/usr --with-stage1-ldflags=\"-specs=/data/data/hilled.pwnterm/files/usr/pwn.spec\" --with-isl-include=/data/data/hilled.pwnterm/files/usr/include --with-isl-lib=/data/data/hilled.pwnterm/files/usr/lib --disable-isl-version-check --disable-tls --enable-host-shared --enable-host-libquadmath --enable-default-pie --with-arch=armv8-a --prefix=/data/data/hilled.pwnterm/files/usr1 --exec-prefix=/data/data/hilled.pwnterm/files/usr1;
+./../gcc/configure --enable-languages=c,c++ --with-system-zlib --disable-multilib --target=aarch64-linux-android --with-gmp=/data/data/hilled.pwnterm/files/usr --with-mpfr=/data/data/hilled.pwnterm/files/usr --with-mpc=/data/data/hilled.pwnterm/files/usr --with-stage1-ldflags=\"-specs=/sdcard/specs\" --with-isl-include=/data/data/hilled.pwnterm/files/usr/include --with-isl-lib=/data/data/hilled.pwnterm/files/usr/lib --disable-isl-version-check --disable-tls --enable-host-shared --enable-host-libquadmath --enable-default-pie --with-arch=armv8-a --prefix=/data/data/hilled.pwnterm/files/usr --exec-prefix=/data/data/hilled.pwnterm/files/usr
 
-# export's
-export CFLAGS_FOR_TARGET=" -I/data/data/hilled.pwnterm/files/usr/include/isl -specs=/data/data/hilled.pwnterm/files/usr/pwn.spec -Os -fbracket-depth=512";
-export CPPFLAGS_FOR_TARGET="-I/data/data/hilled.pwnterm/files/usr/include -I/data/data/hilled.pwnterm/files/usr/include/isl";
-export CXXFLAGS_FOR_TARGET="-specs=/data/data/hilled.pwnterm/files/usr/pwn.spec -Os -fbracket-depth=512";
-export LDFLAGS_FOR_TARGET=" -specs=/data/data/hilled.pwnterm/files/usr/pwn.spec -L/data/data/hilled.pwnterm/files/usr/lib -Wl,-rpath-link,/data/data/hilled.pwnterm/files/usr/lib -Wl,-rpath-link,/data/data/hilled.pwnterm/files/usr/lib";
-export PKG_CONFIG_FOR_TARGET="pkg-config";
-export LD_FOR_BUILD="ld";
-export CXXFLAGS="-fbracket-depth=512";
-export CFLAGS="-fbracket-depth=512";
+export CFLAGS_FOR_TARGET="-specs=/sdcard/specs -I/data/data/hilled.pwnterm/files/usr/include/isl -specs=/sdcard/specs -Os -fbracket-depth=512"
+export CPPFLAGS_FOR_TARGET="-Os -I/data/data/hilled.pwnterm/files/usr/include -I/data/data/hilled.pwnterm/files/usr/include/isl"
+export CXXFLAGS_FOR_TARGET="-Os -fbracket-depth=512"
+export LDFLAGS_FOR_TARGET=" -specs=/sdcard/specs -L/data/data/hilled.pwnterm/files/usr/lib -Wl,-rpath-link,/data/data/hilled.pwnterm/files/usr/lib -Wl,-rpath-link,/data/data/hilled.pwnterm/files/usr/lib"
+export PKG_CONFIG_FOR_TARGET="pkg-config"
 
-# Make
-make all-gcc CFLAGS="-fbracket-depth=512" CXXFLAGS="-fbracket-depth=512";
+export CC="gcc"
+export LD_FOR_BUILD="ld"
+export AR_FOR_TARGET="ar"
+export RANLIB_FOR_TARGET="ranlib"
+export CC_FOR_TARGET="gcc"
+export AS_FOR_TARGET="as"
+export CXX_FOR_TAGET="g++"
+export CPP_FOR_TARGET="cpp"
 
-make install-gcc;
+LD=${LD_FOR_TARGET}
+AR=${AR_FOR_TARGET}
+RANLIB=${RANLIB_FOR_TARGET}
+CC=${CC_FOR_TARGET}
+AS=${AS_FOR_TARGET}
+CXX=${CXX_FOR_TARGET}
+CFLAGS=${CFLAGS_FOR_TARGET} 
+DFLAGS=${LDFLAGS_FOR_TARGET}
+CPPFLAGS=${CPPFLAGS_FOR_TARGET}
+CPP=${CPP_FOR_TARGET} 
+
+# Make gcc
+make all-gcc CFLAGS="-w -fbracket-depth=512 " CXXFLAGS="-w -fbracket-depth=512"
+
